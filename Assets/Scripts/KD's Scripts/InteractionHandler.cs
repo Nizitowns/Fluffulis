2d93f360f3840818079c97a3f31d7c1731d1a0d2
kdeng2001 <85768217+kdeng2001@users.noreply.github.com>
2024-05-21 15:08:25 -0700

    push for 0.2s to push block

--- Assets/Scripts/KD's Scripts/InteractionHandler.cs
+++ Assets/Scripts/KD's Scripts/InteractionHandler.cs
@@ -6,6 +6,10 @@ public class InteractionHandler : MonoBehaviour
 {
     [SerializeField] int groundLayer;
     [SerializeField] int interactableLayer;
+    private float registerPushTime = 0.2f;
+    //private float timeElapsed = 0;
+    //private float timeHit = 0;
+    private Dictionary<UnitBlock, float[]> hits = new Dictionary<UnitBlock, float[]>();
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
         if(hit.gameObject.layer == groundLayer) { return; }
@@ -21,10 +25,31 @@ public class InteractionHandler : MonoBehaviour
             UnitBlock uBlock = hit.transform.parent.GetComponentInChildren<UnitBlock>();
             if (uBlock != null)
             {
+                if(!hits.ContainsKey(uBlock) || (Time.time - hits[uBlock][1]) > registerPushTime) 
+                {
+                    hits[uBlock] = new float[] { 0, Time.time };
+                    //hits[uBlock][0] = 0;
+                    //hits[uBlock][1] = Time.time;
+                }
+                hits[uBlock][0] += Time.deltaTime;
+                if(hits[uBlock][0] < registerPushTime) { return; }
                 uBlock.Push();
+                hits[uBlock][0] = 0;
+                Debug.Log("uBlock push");
+                //if(currentHit != uBlock || ((Time.time - timeHit) > registerPushTime) )
+                //{
+                //    timeElapsed = 0;
+                //    timeHit = Time.time;
+                //    currentHit = uBlock;
+                //}
+                //timeElapsed += Time.deltaTime;
+                //if(timeElapsed < registerPushTime) { return; }
+                //uBlock.Push();
+                //timeElapsed = 0;
                 //Debug.Log("uBlock push");
             }
 
         }
+        Debug.Log("controller collider hit on " + hit.gameObject.name);
     }
 }
