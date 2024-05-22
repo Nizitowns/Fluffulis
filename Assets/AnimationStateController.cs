f074b84f499cea2262a507ac849147bf4af94527
VirtualRook <97001792+VirtualRook@users.noreply.github.com>
2024-05-22 16:22:00 -0700

    Andrews PLaground background and cat animation
    updated post processing, background, and cat animation

---
+++ Assets/AnimationStateController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
+
+
+public class AnimationStateController : MonoBehaviour
+{
+
+    public Animator animator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+        Debug.Log(animator);
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void stoping(CallbackContext ctx)
+    {
+        animator.SetBool("isWalking", false);
+        Debug.Log("stoping");
+    }
+
+    private void walking(CallbackContext ctx)
+    {
+        animator.SetBool("isWalking", true);
+        Debug.Log("walking");
+    }
+
+    private void OnEnable()
+    {
+        if (TryGetComponent(out PlayerInput playerInput))
+        {
+            InputAction moveAction = playerInput.actions["Move"];
+            if (moveAction != null)
+            {
+                moveAction.performed += walking;
+                moveAction.canceled += stoping;
+            }
+       
+        }
+    }
+    private void OnDisable()
+    {
+        
+        if (TryGetComponent(out PlayerInput playerInput))
+        {
+            InputAction moveAction = playerInput.actions["Move"];
+            if (moveAction != null)
+            {
+                moveAction.performed -= walking;
+                moveAction.canceled -= stoping;
+            }
+
+            
+        }
+    }
+
+
+}
