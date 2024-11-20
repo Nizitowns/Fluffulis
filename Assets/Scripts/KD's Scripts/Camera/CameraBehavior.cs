66f9fb0c57b743f99cc534fea713aabb13e341ab
kdeng2001 <85768217+kdeng2001@users.noreply.github.com>
2024-11-19 18:03:39 -0800

    add summary comments to CameraBehavior

--- Assets/Scripts/KD's Scripts/Camera/CameraBehavior.cs
+++ Assets/Scripts/KD's Scripts/Camera/CameraBehavior.cs
@@ -4,7 +4,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using static UnityEngine.InputSystem.InputAction;
-    
+
+/// <summary>
+/// Handles custom camera controls outside of CinemachineFreeLook component,
+/// allowing camera rotation of 45 degrees clockwise or counter-clockwise along the y-axis depending on input.
+/// </summary>
 public class CameraBehavior : MonoBehaviour
 {
     public delegate void BeginPosRotate();
@@ -23,6 +27,10 @@ public class CameraBehavior : MonoBehaviour
     private CinemachineInputProvider vcamInput;
     private GameObject character;
 
+    /// <summary>
+    /// Gets reference to the CinemachineFreeLook and CinemachineInputProvider components,
+    /// and sets default values for properties.
+    /// </summary>
     private void Awake()
     {
         character = GameObject.Find("Character");
@@ -33,6 +41,11 @@ public class CameraBehavior : MonoBehaviour
         rotationDirection = 0;
         targetAngle = cameraAxis.eulerAngles.y;
     }
+    /// <summary>
+    /// Sets up properties to prepare smooth 45 degree rotation based on input,
+    /// while disabling the CinemachineInputProvider input (to ignore default camera control inputs).
+    /// </summary>
+    /// <param name="ctx"> Allows access to player input value. </param>
     void StartRotateOverrideFreeLook(CallbackContext ctx)
     {
         rotationDirection = (int) ctx.ReadValue<float>();
@@ -41,11 +54,14 @@ public class CameraBehavior : MonoBehaviour
         targetAngle = vcam.m_XAxis.Value + rotationDirection * rotateAmount;
         
     }
+    /// <summary>
+    /// Handles smooth rotation towards targetAngle calculated from input and current angle. 
+    /// </summary>
     void RotateOverrideFreeLook() 
     {
         if(vcamInput.enabled == true) { return; }
         timeElapsed += Time.smoothDeltaTime;
-        // Adjust target for wrapping around (btw 180, -180)
+        // Adjust target for wrapping around (between 180, -180).
         if(targetAngle > 180 && vcam.m_XAxis.Value < 0) 
         { 
             targetAngle = -180 + (targetAngle - 180); 
@@ -54,16 +70,24 @@ public class CameraBehavior : MonoBehaviour
         { 
             targetAngle = 180 + (targetAngle + 180); 
         }
-        // rotate to target
+        // Rotate to target.
         vcam.m_XAxis.Value = Mathf.Lerp(vcam.m_XAxis.Value, targetAngle, timeElapsed * rotateSpeed);
-        // enable free camera look after rotation should be done
+
+        // Enable free camera look after rotation is finished.
         if(Mathf.Approximately(targetAngle, vcam.m_XAxis.Value)) { vcamInput.enabled = true; }
     }
+
+    /// <summary>
+    /// Checks for rotation input and rotates towards the angle each frame until reaching the target.
+    /// </summary>
     private void Update()
     {
         RotateOverrideFreeLook();
     }
 
+    /// <summary>
+    /// Subscribes StartRotateOverrideFreeLook to the custom cameraRotation input.
+    /// </summary>
     private void OnEnable()
     {
         if(character.TryGetComponent(out PlayerInput input))
@@ -75,6 +99,10 @@ public class CameraBehavior : MonoBehaviour
             }
         }
     }
+
+    /// <summary>
+    /// Unsubscribes StartRotateOverrideFreeLook from the custom cameraRotation input.
+    /// </summary>
     private void OnDisable()
     {
         if (character.TryGetComponent(out PlayerInput input))
