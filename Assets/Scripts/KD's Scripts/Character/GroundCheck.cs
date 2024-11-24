9b9881e554e92483c2bb835b6b6db64cb80df4f1
kdeng2001 <85768217+kdeng2001@users.noreply.github.com>
2024-11-23 16:44:21 -0800

    add summary comments to GroundCheck

--- Assets/Scripts/KD's Scripts/Character/GroundCheck.cs
+++ Assets/Scripts/KD's Scripts/Character/GroundCheck.cs
@@ -2,16 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// A component for checking if the player is grounded.
+/// </summary>
 public class GroundCheck : MonoBehaviour
 {
     public float radius = 0.22f;
     public LayerMask player = 1 << 9;
+
+    /// <summary>
+    /// Checks if the player is grounded.
+    /// </summary>
+    /// <returns> True if player is touching ground, false otherwise. </returns>
     public bool Grounded()
     {
         bool isGrounded = Physics.CheckSphere(transform.position, radius, ~player, QueryTriggerInteraction.Ignore);
-        return isGrounded;
-        
+        return isGrounded;    
     }
+
+    /// <summary>
+    /// Draws a sphere gizmo, viewable in play mode for debugging.
+    /// </summary>
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
