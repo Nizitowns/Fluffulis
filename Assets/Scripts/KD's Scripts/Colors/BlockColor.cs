38c740fba33add5e96503d4ccd0f4eef0f043319
kdeng2001 <85768217+kdeng2001@users.noreply.github.com>
2024-11-25 16:45:56 -0800

    add summary comment to BlockColor

--- Assets/Scripts/KD's Scripts/Colors/BlockColor.cs
+++ Assets/Scripts/KD's Scripts/Colors/BlockColor.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// A ScriptableObject with an ID property used to distinguish between different types of blocks. 
+/// </summary>
 [CreateAssetMenu(fileName = "BlockColor", menuName = "ScriptableObjects/BlockColor", order = 1)]
 public class BlockColor : ScriptableObject
 {
