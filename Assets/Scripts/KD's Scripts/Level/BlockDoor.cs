32da47c18f2e4a6b9d952ca8ab17a5c101c0af6a
kdeng2001 <85768217+kdeng2001@users.noreply.github.com>
2024-12-01 14:51:08 -0800

    add summary comments to BlockDoor.

--- Assets/Scripts/KD's Scripts/Level/BlockDoor.cs
+++ Assets/Scripts/KD's Scripts/Level/BlockDoor.cs
@@ -2,31 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// A door that opens when activated. It is closed by default and cannot be closed once opened.
+/// </summary>
 public class BlockDoor : Trigger
 {
     List<UnitBlock> blocks;
+    
+    /// <summary>
+    /// Gets a list of blocks that consist of the entire door. 
+    /// </summary>
     private void Start()
     {
         blocks = GetComponentInChildren<BlockContainer>().blocks;
     }
+
+    /// <summary>
+    /// Opens the door.
+    /// </summary>
     public override void Activate()
     {
         Debug.Log("Activate BlockDoor!");
         StartCoroutine(DelayDestroy());
     }
 
+    /// <summary>
+    /// Does nothing. The door cannot be closed once opened.
+    /// </summary>
     public override void DeActivate()
     {
         
     }
 
+    /// <summary>
+    /// Sets each block that makes up the door inactive, allowing entry.
+    /// </summary>
+    /// <returns></returns>
     public IEnumerator DelayDestroy()
     {
         for(int i=blocks.Count-1; i >= 0; i--)
         {
             yield return new WaitForSeconds(0.1f);
             blocks[i].gameObject.SetActive(false);
-            //Debug.Log("destroy " + blocks[i].name);
         }
 
     }
