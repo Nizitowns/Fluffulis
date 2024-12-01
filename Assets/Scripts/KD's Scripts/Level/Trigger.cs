e89881d8b3cf75c8d981eb08a3609555c037d81b
kdeng2001 <85768217+kdeng2001@users.noreply.github.com>
2024-11-30 23:52:25 -0800

    Add summary comments to Trigger.

--- Assets/Scripts/KD's Scripts/Level/Trigger.cs
+++ Assets/Scripts/KD's Scripts/Level/Trigger.cs
@@ -2,8 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// An abstract class for a generic trigger, with activate and deactivate functionality.
+/// </summary>
 public abstract class Trigger: MonoBehaviour
 {
+
+    /// <summary>
+    /// Called when the trigger is activated.
+    /// </summary>
     public abstract void Activate();
+
+    /// <summary>
+    /// Called when the trigger is deactivated.
+    /// </summary>
     public abstract void DeActivate();
 }
