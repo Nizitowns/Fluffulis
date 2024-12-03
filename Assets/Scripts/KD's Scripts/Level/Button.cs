0321c271f4514faf3b6c07ce8e837aebf2c50a8d
kdeng2001 <85768217+kdeng2001@users.noreply.github.com>
2024-12-02 23:23:46 -0800

    Add summary comments to Button.

--- Assets/Scripts/KD's Scripts/Level/Button.cs
+++ Assets/Scripts/KD's Scripts/Level/Button.cs
@@ -1,56 +1,62 @@
 using UnityEngine;
 
+/// <summary>
+/// Handles block - button activation through block color checks.
+/// </summary>
 public class Button : MonoBehaviour
 {
     [Tooltip("Color for comparison when checking correct block. ColorAny by default.")]
     [SerializeField] public BlockColor color;
-    //[Tooltip("Button manager that manages button.")]
-    //[SerializeField] public ButtonManager buttonManager;
     private ButtonManager buttonManager;
     [SerializeField] LayerMask Interactable = 1 << 6;
     [Tooltip("Don't change...")]
     [SerializeField] public BlockColor colorAny;
+
+    /// <summary>
+    /// Events invoked when buttons are activated/deactivated.
+    /// </summary>
     public delegate void ButtonActivated();
     public ButtonActivated buttonActivated;
     public delegate void ButtonDeactivated();
     public ButtonDeactivated buttonDeactivated;
+
     [SerializeField] GameObject effect;
     [SerializeField] int activationEffect;
 
-    //private void Awake()
-    //{
-    //    colorAny = Resources.Load<BlockColor>("BlockColor/ColorAny");
-    //}
-    //private void Start()
-    //{
-    //    //if (color == null) { color = Resources.Load<BlockColor>("ColorAny"); }
-    //    if (buttonManager == null) { GameObject.Find("ButtonManager").TryGetComponent(out buttonManager); }
-    //}
+    /// <summary>
+    /// Activates the button if it's triggered by the correct block.
+    /// </summary>
+    /// <param name="other"></param>
     private void OnTriggerEnter(Collider other)
     {
+        // checks if the collider is interactable
         if ((Interactable & (1 << other.gameObject.layer)) != 0)
         {
+            // checks if collider is a BlockContainer and matches the button color property.
             BlockContainer bC = other.GetComponentInParent<BlockContainer>();
-            //Debug.Log("block container: " + bC.name);
             if (bC != null && bC.color.ID != color.ID && color.ID != colorAny.ID) { return; }
-            //Debug.Log(other.name + " has hit button");
-            //buttonManager.buttonActivated?.Invoke();
+            
+            // activates button
             buttonActivated?.Invoke();
-
             Instantiate(effect, transform.position, effect.transform.rotation);
             AudioManager.Instance.PlaySFX(activationEffect);
         }
     }
+    /// <summary>
+    /// Deactivates the button if the correct block is removed.
+    /// </summary>
+    /// <param name="other"></param>
     private void OnTriggerExit(Collider other)
     {
+        // checks if collider is interactable
         if ((Interactable & (1 << other.gameObject.layer)) != 0)
         {
+            // checks if collider is a BlockContainer and matches the button color property.
             BlockContainer bC = other.GetComponentInParent<BlockContainer>();
             if (bC != null) { if (bC.color.ID != color.ID && color.ID != colorAny.ID) { return; } }
-            //Debug.Log(other.name + " exits button");
-            //buttonManager.buttonDeactivated?.Invoke();
+
+            // deactivates button
             buttonDeactivated?.Invoke();
         }
     }
-    //public void SetButtonManager(ButtonManager bM) { buttonManager = bM; }
 }
