e2d216ddfaa3e35088f160a1e0e269a2c3ecaec0
kdeng2001 <85768217+kdeng2001@users.noreply.github.com>
2024-12-05 14:44:14 -0800

    Add summary comments to Waypoint.

--- Assets/Scripts/KD's Scripts/Level/Waypoint.cs
+++ Assets/Scripts/KD's Scripts/Level/Waypoint.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Waypoint helps visualize waypoints when debugging by drawing a sphere around the waypoint.
+/// </summary>
 public class Waypoint : MonoBehaviour
 {
-
+    /// <summary>
+    /// Draws a sphere of size 0.5f around the transform's position.
+    /// </summary>
     private void OnDrawGizmos()
     {
         Gizmos.DrawSphere(transform.position, .5f);
