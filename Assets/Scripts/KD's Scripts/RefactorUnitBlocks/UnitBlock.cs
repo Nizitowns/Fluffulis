724f0adbc23504815c8deeb739d94e5266e043ff
kdeng2001 <85768217+kdeng2001@users.noreply.github.com>
2024-12-07 16:26:17 -0800

    remove commented code from UnitBlock

--- Assets/Scripts/KD's Scripts/RefactorUnitBlocks/UnitBlock.cs
+++ Assets/Scripts/KD's Scripts/RefactorUnitBlocks/UnitBlock.cs
@@ -28,15 +28,9 @@ public class UnitBlock : MonoBehaviour
 
         
     }
-
-    //public delegate void ReceivePush(UnitBlock block);
-    //public ReceivePush receivePush;
-    //public void Push() { receivePush?.Invoke(this); }
     public void Push() { currentContainer.ReceivePush(this); }
     public Vector3 GetPushDirection()
     {
-        //if (!isPushable) { return; }
-        //if (pushing) { return; }
         Vector3[] directions = { Vector3.forward, -Vector3.forward, Vector3.right, -Vector3.right };
         foreach (Vector3 dir in directions)
         {
@@ -50,7 +44,6 @@ public class UnitBlock : MonoBehaviour
 
     private bool IsPushDirection(Vector3 direction)
     {
-        //Debug.Log("Checking push direction: " + direction);
         RaycastHit[][] hits;
         if (Mathf.Approximately(direction.x, 0))
         {
@@ -77,10 +70,8 @@ public class UnitBlock : MonoBehaviour
         {
             for(int j=0; j<hits[i].Length; j++)
             {
-                //Debug.Log(hits[i][j].transform.name + ", " + hits[i][j].transform.root.name + " was hit");
                 if (hits[i][j].transform.CompareTag("Player")) 
                 {
-                    //Debug.Log(hits[i][j].transform.name + ", " + hits[i][j].transform.root.name + " was hit");
                     return true; 
                 }
                 
@@ -88,35 +79,6 @@ public class UnitBlock : MonoBehaviour
         }
         return false;
     }
-    //public bool IsBlocked(Vector3 direction)
-    //{
-    //    //Debug.Log(name + " is being checked for block (UnitBlock)");
-    //    Debug.DrawRay(transform.position, direction * 1.2f, Color.red);
-
-    //    if (Physics.Raycast(transform.position, direction, 1.2f, BlockLayers, QueryTriggerInteraction.Collide)) 
-    //    { 
-    //        foreach(RaycastHit hit in Physics.RaycastAll(transform.position, direction, 1.2f, BlockLayers, QueryTriggerInteraction.Collide))
-    //        {
-    //            //Debug.Log("hits: " + hit.transform.name);
-    //            if(hit.transform.gameObject.layer == 2) 
-    //            { 
-    //                //Debug.Log(name + "is not blocked by " + hit.transform.name + " " + hit.transform.gameObject.layer); 
-    //                return false; 
-    //            }
-    //            if (hit.transform.gameObject.GetComponentInParent<BlockContainer>() == currentContainer) 
-    //            {
-
-    //                return false; 
-    //            }
-    //            //Debug.Log(name + "is not blocked by " + hit.transform.name);
-    //        }
-    //        //return false;
-    //        //Debug.Log(name + " is blocked...");
-    //        return true;
-    //    }
-    //    //Debug.Log(name + "is not blocked!!!");
-    //    return false;
-    //}
     public bool IsBlocked(Vector3 direction)
     {
         RaycastHit[] hits = Physics.RaycastAll(transform.position, direction);
@@ -128,21 +90,15 @@ public class UnitBlock : MonoBehaviour
             { 
                 continue; 
             }
-            //Debug.Log(hits[i].transform.name + ", " + hits[i].transform.root.name + "is blocking " + name);
             return true;
         }
         return false;
     }
     public void Snap(Vector3 target)
     {
-        //Debug.Log("Snap");
         transform.position = grid.WorldToCell(target);
-        //meshRenderer.transform.localPosition = blockCollider.transform.localPosition = Vector3.zero;
-        //groundCheck.transform.localPosition = Vector3.down * 0.5f;
         meshRenderer.transform.position = grid.WorldToCell(target);
         blockCollider.transform.position = grid.WorldToCell(target);
-        //Debug.Log(transform.parent.name + " snaps to " + grid.WorldToCell(target));
         groundCheck.transform.position = grid.WorldToCell(target) + Vector3.down * 0.5f;
-        //groundCheck.transform.position = transform.position + Vector3.down * 0.5f;
     }
 }
