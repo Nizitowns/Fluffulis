cf45ea7b4e0b6f804df186a089a98c241c840693
kdeng2001 <85768217+kdeng2001@users.noreply.github.com>
2024-12-10 18:42:41 -0800

    remove commented code from UnitBlockGravity

--- Assets/Scripts/KD's Scripts/RefactorUnitBlocks/UnitBlockGravity.cs
+++ Assets/Scripts/KD's Scripts/RefactorUnitBlocks/UnitBlockGravity.cs
@@ -5,20 +5,16 @@ using UnityEngine;
 public class UnitBlockGravity : MonoBehaviour
 {
     UnitBlock block;
-    //public delegate void ReceiveGravity();
-    //public ReceiveGravity receiveGravity;
     private void Awake()
     {
         block = gameObject.GetComponentInParent<UnitBlock>();
     }
     private void Start()
     {
-        //receiveGravity?.Invoke();
         block.currentContainer.ReceiveGravity();
     }
     private void OnTriggerExit(Collider other)
     {
-        //receiveGravity?.Invoke();
         Debug.Log(name + "exits " + other.name + " and " + block.currentContainer.transform.name + "receives gravity");
         block.currentContainer.ReceiveGravity();
     }
