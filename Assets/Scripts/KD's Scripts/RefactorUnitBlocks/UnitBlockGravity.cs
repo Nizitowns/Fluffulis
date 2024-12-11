9cd117e114a70085f63bf51646cfa8911e1efe5f
kdeng2001 <85768217+kdeng2001@users.noreply.github.com>
2024-12-10 20:05:31 -0800

    add summary comments to UnitBlockGravity

--- Assets/Scripts/KD's Scripts/RefactorUnitBlocks/UnitBlockGravity.cs
+++ Assets/Scripts/KD's Scripts/RefactorUnitBlocks/UnitBlockGravity.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class UnitBlockGravity : MonoBehaviour
 {
     UnitBlock block;
+    /// <summary>
+    /// Finds the UnitBlock component associated with this component.
+    /// </summary>
     private void Awake()
     {
         block = gameObject.GetComponentInParent<UnitBlock>();
     }
+    /// <summary>
+    /// Attempts to apply gravity at the start of the scene.
+    /// </summary>
     private void Start()
     {
         block.currentContainer.ReceiveGravity();
     }
+    /// <summary>
+    /// Attempts to apply gravity whenever the UnitBlock exits contact with another collider.
+    /// </summary>
+    /// <param name="other"> The other collider that the UnitBlock exits contact from. </param>
     private void OnTriggerExit(Collider other)
     {
         block.currentContainer.ReceiveGravity();
